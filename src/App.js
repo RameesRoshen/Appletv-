36150cf50fe847b1a9581cb676e492f8d65fb525
RameesRoshen <ramizroshen4510@gamil.com>
2023-11-18 19:17:31 +0530

    appletv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Banner from './component/Banner';
+import Content from './component/Content';
+import Header from './component/Header';
+import Notice from './component/Notice';
+import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Header/>
+      <Banner/>
+
+      
+    
+    
+      <Content isPresent={true} title="Watch Premieres For Free" fetchUrl={requests.fetchNetflixOriginals}/>
+
+<Content isPresent={true} title="Newest Releases" fetchUrl={requests.fetchTrending}/>
+
+<Content  isPresent={true} title="Epic Sci-fi" fetchUrl={requests.fetchTopRated}/>
+
+<Notice fetchUrl={requests.fetchNetflixOriginals}/>
+
+<Content isPresent={true} title="All Drama Series" fetchUrl={requests.fetchActionMovies}/>
+
+<Content isPresent={true} title=" ALL Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+
+<Content isPresent={true} title="All Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+
+
+
+
     </div>
   );
 }
